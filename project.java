85ba59077599828b1ebf23dfc3303c682f0365e0
alexcostello2 <129113456+alexcostello2@users.noreply.github.com>
2023-04-22 12:06:56 -0400

    Update project.java

--- project.java
+++ project.java
@@ -1,7 +1,7 @@
 public class project{
     public static void main(String[] args){
         // This will be the file that is ran.
-        gui myGUI = new gui();
+        gui main = new gui();
 
         
     }
